d6a63e128f887a7455cd32e759399f38d15ed05b
AmaraABC <amara-noah.gakou@efrei.net>
2025-02-25 23:42:45 +0100

    Enhanced JS file

--- n°1/script/main.js
+++ n°1/script/main.js
@@ -1,1 +1,8 @@
 // Nouveau : Fichier JavaScript pour le bon fonctionnement du menu hamburger
+const burger = document.querySelector('.day-one-burger');
+const menu = document.querySelector('.day-one-nav');
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    menu.classList.toggle('active');
+});
